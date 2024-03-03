2d14d7537b0655e0db3283a99b7dad9cd49c8eef
Matheus Amorim <136996208+amorimsf@users.noreply.github.com>
2024-03-03 12:49:55 -0300

    primero ex do curso de JS

---
+++ aula04/script.js
@@ -0,0 +1,5 @@
+window.alert('Minha primera mensagem!') // Janela de alerta
+
+window.confirm('Está gostando de JS?') //Janela de confirmação
+
+window.prompt('Qual é seu nome?') // Janela de texto 
